e3d53f892d223b38c6c42703a0ff1651be9f84e4
Kristea Gamel <krgamel@gmail.com>
2019-03-03 12:34:25 -0600

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'ByKristea',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Figuring life out as I go</h1>
+    <p>Hello Friends! Thanks for stopping by.</p>
+    <p>Browse around to see what I am up to, projects I am working on, or to learn a little more about me.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage

649904a0f2a6abd7b0b6b9e054a240cbb408d875
Kristea Gamel <krgamel@gmail.com>
2019-03-03 15:21:32 -0600

    first section design

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Figuring life out as I go</h1>
-    <p>Hello Friends! Thanks for stopping by.</p>
-    <p>Browse around to see what I am up to, projects I am working on, or to learn a little more about me.</p>
-    <Link to="/page-2/">Let's be friends</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+      <h1>Figuring life out as I go</h1>
+        <p>Hello Friends, thanks for stopping by. Browse around to see what I am up to, projects I'm working on, or to learn a little more about me.</p>
+      <Link to="/page-2/">Let's be friends</Link> 
+      </div>
+    </div>
+   
   </div>
 )
 

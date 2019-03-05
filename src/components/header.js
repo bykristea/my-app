8c30f5979e73ed3aa12060e902e60d911fba4461
Kristea Gamel <krgamel@gmail.com>
2019-03-04 22:38:04 -0600

    header components and some animation

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,20 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.css'
+
+
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="/"><img src={require('../img/logo.svg')} width="50" /></Link>
+      <Link to="/Projects">Projects</Link>
+      <Link to="/Life">Life</Link>
+      <Link to="/About Me">About Me</Link>
+      <Link to="/Contact"><button>Reach Out</button></Link>
+
     </div>
+    
   </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,13 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
-      <h1>Figuring life out as I go</h1>
+      <h1>Figuring life <br />out as I go</h1>
         <p>Hello Friends, thanks for stopping by. Browse around to see what I am up to, projects I'm working on, or to learn a little more about me.</p>
       <Link to="/page-2/">Let's be friends</Link> 
+      <svg width="2560" height="172" viewBox="0 0 2560 172" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z" fill="white"/>
+      </svg>
+
       </div>
     </div>
    
